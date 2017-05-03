f356a6b3f3aeadab570a9c4580a4e6bf47a3b28a
Plínio Balduino <pbalduino+github@gmail.com>
2017-05-03 20:55:37 -0300

    Reads dockerd events as JSON #9

--- pkg/docker/docker.go
+++ pkg/docker/docker.go
@@ -1,11 +1,35 @@
 package docker
 
-import "log"
-
-type client struct {
-}
+import (
+	"bufio"
+	"context"
+	"log"
+	"net"
+	"net/http"
+	"strings"
+)
 
 // Start does
 func Start(socket string) {
 	log.Println("Hello docker")
+	parts := strings.Split(socket, "://")
+
+	client := http.Client{
+		Transport: &http.Transport{
+			DialContext: func(_ context.Context, _, _ string) (net.Conn, error) {
+				return net.Dial("unix", parts[1])
+			},
+		},
+	}
+
+	resp, _ := client.Get("http://docker/events")
+	reader := bufio.NewReader(resp.Body)
+
+	log.Println(".")
+	for {
+		line, _ := reader.ReadBytes('\n')
+
+		log.Println(string(line))
+	}
+
 }
